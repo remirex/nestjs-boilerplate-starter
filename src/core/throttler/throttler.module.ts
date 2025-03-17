a2a02b34702e912e1c3592a219cdd3ed4f42ba47
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2025-03-17 18:06:41 +0100

    Lint fix

--- src/core/throttler/throttler.module.ts
+++ src/core/throttler/throttler.module.ts
@@ -3,16 +3,16 @@ import { ApiConfigService } from '../config/providers/config.service';
 import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-    imports: [
-        ThrottlerModule.forRootAsync({
-            inject: [ApiConfigService],
-            useFactory: (config: ApiConfigService) => [
-              {
-                ttl: config.rateLimit.ttl,
-                limit: config.rateLimit.limit,
-              },
-            ],
-          })
-    ]
+  imports: [
+    ThrottlerModule.forRootAsync({
+      inject: [ApiConfigService],
+      useFactory: (config: ApiConfigService) => [
+        {
+          ttl: config.rateLimit.ttl,
+          limit: config.rateLimit.limit,
+        },
+      ],
+    }),
+  ],
 })
 export class ApiThrottlerModule {}
